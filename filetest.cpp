7cec54afa8d617acbf28c17a35e92fe7ae9b4909
Ethan Lusterman <ethan.lusterman@gmail.com>
2013-03-18 06:40:31 -0400

    moved files to repo

---
+++ filetest.cpp
@@ -0,0 +1,34 @@
+//
+//  filetest.cpp
+//
+//  Created by Ethan Lusterman on 3/17/13.
+//	The Cooper Union, Electrical Engineering Dept
+//	ECE164 Data Structures & Algorithms I, Spring 2013
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string line;
+	string inFile;
+	
+	cout << "Enter name of input file: ";
+	cin >> inFile;
+	
+	// input file stream
+	ifstream inData(inFile.c_str());
+	
+	if ( inData.is_open() ) {
+		while ( getline(inData,line) ) {
+			cout << line << endl;
+		}
+	}
+	else {
+		cout << inFile << " is not a valid filename!" << endl;
+	}
+	
+	return 0;
+}
